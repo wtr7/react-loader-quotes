6397a5a901ce873a4df381b5386c847e0f0ea6e6
wtr7 <info@woutervanrespaille.be>
2016-12-21 18:15:23 +0700

    Add quotes and animations

--- src/ReactLoaderQuotes.js
+++ src/ReactLoaderQuotes.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-class ReactLoaderQuotes extends React.Component {
+import {
+  setContainer,
+  setBackground,
+  setQuotes,
+  setTitle,
+  setLoader
+} from './style';
 
+class ReactLoaderQuotes extends React.Component {
   static get propTypes() {
     return {
-      quotes: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
+      loading: React.PropTypes.bool.isRequired,
+      quotes: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
+      random: React.PropTypes.bool,
+      speed: React.PropTypes.number,
+      backgroundColorLoader: React.PropTypes.string,
+      backgroundColorQuotes: React.PropTypes.string,
+      backgroundColorBackground: React.PropTypes.string,
+      opacityBackground: React.PropTypes.number,
+      fontSizeTitle: React.PropTypes.string,
+      colorTitle: React.PropTypes.string,
+      fontFamilyTitle: React.PropTypes.string
     };
   }
 
@@ -12,62 +29,72 @@ class ReactLoaderQuotes extends React.Component {
     super(props);
 
     this.state = {
-      styles: {
-        container: {
-          position: 'fixed',
-          left: 0,
-          width: '100%',
-          height: '100%',
-        },
-        background: {
-          position: 'fixed',
-          left: 0,
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'black',
-          opacity: 0.5,
-          zIndex: 99999
-        },
-        quotes: {
-          position: 'relative',
-          top: '50%',
-          width: 'auto',
-          height: 50,
-          maxWidth: 200,
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          backgroundColor: 'black',
-          transform: "translateY(-50%)",
-          zIndex: 999999
-        },
-        title: {
-          fontSize: 14,
-          color: 'white'
-        },
-        loader: {
-          position: 'absolute',
-          width: '100%',
-          height: 5,
-          backgroundColor: 'yellow',
-          bottom: 0
-        }
-      }
+      currentQuotesIndex: 0
     };
+
+    this._startQuotes();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.loading !== nextProps.loading &&
+        !nextProps.loading) {
+      clearInterval(this.quotesInterval);
+    }
+  }
+
+  _getNextQuote(quotes) {
+    // if at the end of our quotes, then go back to the first quote
+    if (this.state.currentQuotesIndex === quotes.length - 1) {
+      return 0;
+    } else {
+      return this.state.currentQuotesIndex + 1;
+    }
+  }
+
+  _getRandomQuote(quotes) {
+    return Math.floor(Math.random() * quotes.length);
+  }
+
+  _startQuotes() {
+    const quotes = this.props.quotes;
+    const speedInMilliseconds = this.props.speed * 1000;
+
+    this.quotesInterval = setInterval(() => {
+      this.setState({
+        currentQuotesIndex: (this.props.random) ? this._getRandomQuote(quotes) : this._getNextQuote(quotes)
+      });
+    }, speedInMilliseconds);
   }
 
   render() {
+    const Loader = setLoader(this.props);
+    const Quotes = setQuotes(this.props);
+    const Container = setContainer(this.props);
+    const Background = setBackground(this.props);
+    const Title = setTitle(this.props);
+
     return (
-      <div style={this.state.styles.container}>
-        <div style={this.state.styles.background}>
-        </div>
-        <div style={this.state.styles.quotes}>
-          <p style={this.state.styles.title}>Titel</p>
-          <div style={this.state.styles.loader}></div>
-        </div>
-      </div>
+      <Container>
+        <Background />
+        <Quotes>
+          <Title>{this.props.quotes[this.state.currentQuotesIndex]}</Title>
+          <Loader></Loader>
+        </Quotes>
+      </Container>
     );
   }
-
 }
+
+ReactLoaderQuotes.defaultProps = {
+  random: false,
+  speed: 1.5,
+  backgroundColorLoader: 'yellow',
+  backgroundColorQuotes: 'black',
+  backgroundColorBackground: 'black',
+  opacityBackground: 0.5,
+  fontSizeTitle: '18px',
+  colorTitle: 'white',
+  fontFamilyTitle: 'Arial'
+};
 
 export default ReactLoaderQuotes;
---
+++ src/style.js
@@ -0,0 +1,82 @@
+import styled, { keyframes } from 'styled-components';
+
+const setContainer = ({loading}) => {
+  const display = !loading ? 'none' : 'inline-block';
+
+  return styled.div`
+    position: fixed;
+    left: 0;
+    text-align: center;
+    width: 100%;
+    height: 100%;
+    z-index: 99999;
+    display: ${display}
+  `;
+};
+
+const setBackground = ({backgroundColorBackground, opacityBackground}) => {
+  return styled.div`
+    position: fixed;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${backgroundColorBackground};
+    opacity: ${opacityBackground};
+  `;
+};
+
+const setQuotes = ({backgroundColorQuotes}) => {
+  return styled.div`
+    position: relative;
+    top: 50%;
+    display: inline-block;
+    width: auto;
+    height: auto;
+    max-width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: ${backgroundColorQuotes};
+    transform: translateY(-50%);
+    z-index: 999999;
+  `;
+};
+
+const setTitle = ({fontSizeTitle, colorTitle, fontFamilyTitle}) => {
+  return styled.p`
+    text-align: center;
+    font-family: ${fontFamilyTitle};
+    font-size: ${fontSizeTitle};
+    padding: 10px;
+    color: ${colorTitle};
+    margin-bottom: 0px;
+  `;
+};
+
+const transition = keyframes`
+  from {
+    width: 0%;
+  }
+
+  to {
+    width: 100%;
+  }
+`;
+
+const setLoader = ({speed, backgroundColorLoader}) => {
+  return styled.div`
+    position: absolute;
+    width: 0%;
+    height: 5px;
+    background-color: ${backgroundColorLoader};
+    bottom: 0px;
+    animation: ${transition} ${speed}s ease-out infinite;
+  `;
+};
+
+export {
+  setContainer,
+  setBackground,
+  setQuotes,
+  setTitle,
+  setLoader
+};
